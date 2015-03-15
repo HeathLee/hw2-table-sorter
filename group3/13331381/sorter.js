08b73d9c37983ae5e6c577e48a3c7046f730400a
zhushh <zsh5318@gmail.com>
2015-03-16 01:09:05 +0800

    change sorter.js and sorter.css to be cleaner

--- group3/13331381/sorter.js
+++ group3/13331381/sorter.js
@@ -17,71 +17,50 @@ function getAllTables() {
 function makeAllTablesSortable(tables) {
     // make a table sortable
     function makeTableSortable(table) {
-        // render table head
-        function renderHead(table, obj) {
-            var theads = table.getElementsByTagName("th");
-            for (var i = 0; i < theads.length; i++) {
-                if (theads[i] != obj) {
-                    theads[i].style.backgroundColor = "#000080";
-                }
-            }
-            if (obj.className == "" || obj.className == "descend") {
-                obj.style.background = "#a4b0fd url(ascend.png) no-repeat right";
-            } else if (obj.className == "ascend") {
-                obj.style.background = "#a4b0fd url(descend.png) no-repeat right";
-            }
-        }
-
         // sort table by given the table, thead, and column
         function sortTable(table, obj, index) {
-            // exchange first's and second's html
-            function exchangeInnerHtml(first, second) {
-                var temp = first.innerHTML;
-                first.innerHTML = second.innerHTML;
-                second.innerHTML = temp;
-            }
-
-            // determine if row a and row b should exchange 
-            function shouldExchange(a, b, ascend) {
-                if (ascend) {
-                    return a.innerHTML < b.innerHTML;
-                } else {
-                    return a.innerHTML > b.innerHTML;
+            var rows = table.rows;
+            for (var i = 1; i < rows.length - 1; i++) {
+                for (var j = i+1; j < rows.length; j++) {
+                    if (rows[i].cells[index].innerHTML < rows[j].cells[index].innerHTML) {
+                        var t = rows[i].innerHTML;
+                        rows[i].innerHTML = rows[j].innerHTML;
+                        rows[j].innerHTML = t;
+                    }
                 }
             }
 
-            var rows = table.rows;
-            var ascend = (obj.className == "" || obj.className == "descend");     // alert(ascend);
-            if (obj.className == "ascend") ascend = false;
-
-            // sort algorithm
-            for (var i = 1; i < rows.length - 1; i++) {
-                for (var j = i + 1; j < rows.length; j++) {
-                    if ( shouldExchange(rows[j].cells[index], rows[i].cells[index], ascend) ) {
-                        exchangeInnerHtml(rows[j], rows[i]);
-                    }
+            // reverse case
+            if (obj.className == "descend") {
+                var l = parseInt(rows.length / 2 + 1);
+                for (var i = 1; i < l; i++) {
+                    var t = rows[i].innerHTML;
+                    rows[i].innerHTML = rows[rows.length - i].innerHTML;
+                    rows[rows.length - i].innerHTML = t;
                 }
             }
         }
 
         // change className
-        function changeClassName(obj) {
-            if (obj.className == "") {
-                obj.className = "ascend";
-            } else if (obj.className == "ascend") {
+        function changeClassName(table, obj) {
+            var ths = table.getElementsByTagName("th");
+            for (var i = 0; i < ths.length; i++) {
+                if (ths[i] != obj)
+                    ths[i].className = "no_active";
+            }
+            if (obj.className == "ascend") {
                 obj.className = "descend";
-            } else if (obj.className == "descend") {
+            } else {
                 obj.className = "ascend";
             }
         }
 
-        var theads = table.getElementsByTagName("th");
+        var theads = table.rows[0].cells;
         for (var i = 0; i < theads.length; i++) {
             theads[i].onclick = function (a, b, c) {
                 return function () {
+                    changeClassName(a, b);
                     sortTable(a, b, c);
-                    renderHead(a, b);
-                    changeClassName(b);
                 }
             }(table, theads[i], i);
         }
@@ -92,5 +71,3 @@ function makeAllTablesSortable(tables) {
         makeTableSortable(tables[i]);
     }
 }
-
-
